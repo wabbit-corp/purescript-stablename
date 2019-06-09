aa0b9bc26ad092a04f387b68780ddd361bc6ac2b
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2019-06-09 11:15:57 -0600

    More docs.

--- src/Effect/StableName.purs
+++ src/Effect/StableName.purs
@@ -4,12 +4,33 @@ import Prelude (class Eq)
 import Effect (Effect)
 import Data.Hashable (class Hashable)
 
+{-|
+  An abstract name for an object, that supports equality and hashing.
+
+  Stable names have the following property:
+
+  * If @sn1 :: StableName@ and @sn2 :: StableName@ and @sn1 == sn2@
+   then @sn1@ and @sn2@ were created by calls to @makeStableName@ on
+   the same object.
+
+  The reverse is not necessarily true: if two stable names are not
+  equal, then the objects they name may still be equal.  Note in particular
+  that `makeStableName` may return a different `StableName` after an
+  object is evaluated.
+-}
 foreign import data StableName :: Type -> Type
 
+-- | Makes a 'StableName' for an arbitrary object.
 foreign import makeStableName :: forall a. a -> Effect (StableName a)
 
-foreign import eqStableName :: forall a. StableName a -> StableName a -> Boolean
+-- | Equality on 'StableName' that does not require that the types of
+-- the arguments match.
+foreign import eqStableName :: forall a. StableName a -> StableName b -> Boolean
 
+-- | Convert a 'StableName' to an 'Int'.  The 'Int' returned is not
+-- necessarily unique; several 'StableName's may map to the same 'Int'
+-- (in practice however, the chances of this are small, so the result
+-- of 'hashStableName' makes a good hash key).
 foreign import hashStableName :: forall a. StableName a -> Int
 
 instance stableNameEq :: Eq (StableName a) where
